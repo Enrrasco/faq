96b0bc0ab5d60f26fba204c87f6e2c5361165054
ercu <ec97@njit.edu>
2018-04-28 01:56:23 -0400

    Create answer form and question form, link both; been able to ask questions.

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -29,9 +29,10 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question;
+        $edit = FALSE;
+        return view('questionForm', ['question' => $question,'edit' => $edit  ]);
     }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -40,9 +41,19 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+        ]);
+        $input = request()->all();
+        $question = new Question($input);
+        $question->user()->associate(Auth::user());
+        $question->save();
 
+        return redirect()->route('home')->with('message', 'IT WORKS!');
+    }
     /**
      * Display the specified resource.
      *
