19c9fad7c5006b750b27c62bfa187fc60bd88407
ercu <ec97@njit.edu>
2018-04-30 18:53:17 -0400

    Final push

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.15 on 2018-04-28 01:57:44.
+ * Generated for Laravel 5.6.15 on 2018-04-29 02:54:18.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
